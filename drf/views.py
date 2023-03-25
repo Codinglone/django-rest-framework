239166521476dff897961150146de3823eb0f518
Fabrice <codinglone@gmail.com>
2023-03-25 19:20:09 +0200

    Added authentication

--- drf/views.py
+++ drf/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from drfapp.serializers import StudentSerializer
 from drfapp.models import Student 
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         qs = Student.objects.all()
-        serializer = StudentSerializer(qs, many=True)
-        return Response(data)
+        student1 = qs.first()
+        serializer = StudentSerializer(student1)
+        return Response(serializer.data)
     def post(self, request, *args, **kwargs):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
